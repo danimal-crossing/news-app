f45df577b099ff7c8bb6a14ea98248671dc7a67c
dani <dani.zaghian@gmail.com>
2020-06-16 10:58:42 -0700

    adds credentials

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,7 +1,7 @@
 class Api::ArticlesController < ApplicationController
 
   def index
-    @articles = HTTP.auth("Bearer <your-api-key>").get("http://newsapi.org/v2/everything?q=bitcoin")
+    @articles = HTTP.auth("Bearer #{Rails.application.credentials.news_api[:api_key]}").get("http://newsapi.org/v2/everything?q=bitcoin")
     render json: @articles.parse
   end
 
